fe94af9f8d88f42c7df9eaaca385b3299e9dea85
Anton Giese <antongiese98@gmail.com>
2022-05-05 13:26:57 +0200

    start popup

--- app/src/main/java/es/gidm/backstack/MisPalabras.java
+++ app/src/main/java/es/gidm/backstack/MisPalabras.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.RadioGroup;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.view.Gravity;
+
 
 public class MisPalabras extends AppCompatActivity {
 
@@ -54,6 +60,7 @@ public class MisPalabras extends AppCompatActivity {
             overridePendingTransition(0, 0);
             break;
           case R.id.ajustes:
+
             Log.i("matching", "matching inside1 rate" + checkedId);
             in = new Intent(getBaseContext(),Actividad3.class);
             startActivity(in);
@@ -64,5 +71,31 @@ public class MisPalabras extends AppCompatActivity {
         }
       }
     });
+  }
+  public void onButtonShowPopupWindowClick(View view) {
+
+    // inflate the layout of the popup window
+    LayoutInflater inflater = (LayoutInflater)
+            getSystemService(LAYOUT_INFLATER_SERVICE);
+    View popupView = inflater.inflate(R.layout.popup_lengua, null);
+
+    // create the popup window
+    int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+    int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+    boolean focusable = true; // lets taps outside the popup also dismiss it
+    final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+    // show the popup window
+    // which view you pass in doesn't matter, it is only used for the window tolken
+    popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+    // dismiss the popup window when touched
+//    popupView.setOnTouchListener(new View.OnTouchListener() {
+//      @Override
+//      public boolean onTouch(View v, MotionEvent event) {
+//        popupWindow.dismiss();
+//        return true;
+//      }
+//    });
   }
 }
