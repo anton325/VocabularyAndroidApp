3c2a1c231961cae16356704ff7ab2af0ed6eabca
Anton Giese <antongiese98@gmail.com>
2022-05-09 13:19:58 +0200

    incorporated recycleview in palabras, but lost the toolbar

--- app/src/main/java/es/gidm/backstack/Palabras.java
+++ app/src/main/java/es/gidm/backstack/Palabras.java
@@ -4,29 +4,57 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
-public class Palabras extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Palabras extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+  MyRecyclerViewAdapter adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    Log.i("palabras","started palabras");
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_actividad3);
+    setContentView(R.layout.palabras_style);
 
-/*
-    Toolbar toolbar = findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    FloatingActionButton fab = findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
-*/
+    // data to populate the RecyclerView with
+    ArrayList<String> animalNames = new ArrayList<>();
+    animalNames.add("Horse");
+    animalNames.add("Cow");
+    animalNames.add("Camel");
+    animalNames.add("Sheep");
+    animalNames.add("Goat");
+    animalNames.add("Horse");
+    animalNames.add("Cow");
+    animalNames.add("Camel");
+    animalNames.add("Sheep");
+    animalNames.add("Goat");
+    animalNames.add("Horse");
+    animalNames.add("Cow");
+    animalNames.add("Camel");
+    animalNames.add("Sheep");
+    animalNames.add("Goat");
+    animalNames.add("Horse");
+    animalNames.add("Cow");
+    animalNames.add("Camel");
+    animalNames.add("Sheep");
+    animalNames.add("Goat");
 
+    // set up the RecyclerView
+    RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    adapter = new MyRecyclerViewAdapter(this, animalNames);
+    adapter.setClickListener(this);
+    recyclerView.setAdapter(adapter);
+  }
+  @Override
+  public void onItemClick(View view, int position) {
+    Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
   }
 
 }
